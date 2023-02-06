5f7a969b9ef2ce7ccb92fbbec7441c22dd2042dc
skarltjr <kisa0828@naver.com>
2023-02-06 21:07:46 +0900

    chore : 자동차 객체

---
+++ src/test/java/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private static final int START_LOCATION = 1;
+    private int location;
+    private String carName;
+
+    public Car(String carName) {
+        this.location = START_LOCATION;
+        this.carName = carName;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+}
